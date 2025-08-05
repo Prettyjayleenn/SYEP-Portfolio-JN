b505dad3ddb47c0c2a8364bf38e0afe38ee4d659
Prettyjayleenn <jayleenn0914@gmail.com>
2025-08-05 19:23:06 +0000

    Added a test imonial page

--- script.js
+++ script.js
@@ -1,3 +1,87 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'Susan Smith',
+        img: '#',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+
+{    id: 2,
+        name: 'Anna Smith',
+        img: '#',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {    id: 3,
+        name: 'Peter Jones',
+        img: '#',
+        text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+];
+
+
+
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentItem = 0;
+window.addEventListener('DOMContentLoaded', function(){
+    const item = reviews [currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+});
+
+function showPerson(person){
+    const item = reviews[person];
+     img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+nextBtn.addEventListener('click', function(){
+ currentItem++;
+  if (currentItem > reviews.length -1){
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', function (){
+   currentItem--;
+   if(currentItem < 0){
+      currentItem = reviews.length -1;
+   }
+  showPerson(currentItem);
+});
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log("Hello World");
 
 let chicken = 123;
